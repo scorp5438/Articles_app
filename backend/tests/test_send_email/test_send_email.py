e0cbd0210a4014b0b6ff7f70dd0f4797f78f7923
alex <alex_77_90@mail.ru>
2025-05-31 19:47:04 +0500

    test v 0.2.5

--- backend/tests/test_send_email/test_send_email.py
+++ backend/tests/test_send_email/test_send_email.py
@@ -14,7 +14,7 @@ async def test_send_email_real_smtp(override_smtp_config, clear_mailhog):
         full_name='New Test User'
     )
 
-    response = requests.get('http://localhost:8025/api/v2/messages')
+    response = requests.get('http://0.0.0.0:8025/api/v2/messages')
     if response.status_code != 200:
         print("MailHog is not reachable.")
 
@@ -27,7 +27,7 @@ async def test_send_email_real_smtp(override_smtp_config, clear_mailhog):
 
     await asyncio.sleep(5)
 
-    response = requests.get('http://localhost:8025/api/v2/messages')
+    response = requests.get('http://0.0.0.0:8025/api/v2/messages')
     messages = response.json()
     last_message = messages["items"][0]
     recipients = last_message.get('Content').get('Headers').get('To')
